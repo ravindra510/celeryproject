6ed82c20841fccecda48bb080ca0f41eb431783b
ravindra510 <prajapatiravindra072@gmail.com>
2024-12-28 15:25:24 +0530

    changes done

--- celeryapp/views.py
+++ celeryapp/views.py
@@ -17,13 +17,21 @@ class ProductListCreateView(mixins.ListModelMixin,
     def post(self, request, *args, **kwargs):
         return self.create(request, *args, **kwargs)
 
+import os
+
 class ProductImportExcelView(generics.GenericAPIView):
     def post(self, request, *args, **kwargs):
         file = request.FILES['file']
-        file_path = f"C:\\Users\\hp\\Downloads\\{file.name}"
+        
+        # Construct the file path using os.path.join
+        file_path = os.path.join(r"C:\Users\Rahul\Downloads", file.name)
+
+        # Save the uploaded file to the specified path
         with open(file_path, 'wb') as f:
             for chunk in file.chunks():
                 f.write(chunk)
-        # Trigger the Celery task
+
+        # Trigger the Celery task to process the uploaded file
         import_excel_data.delay(file_path)
+
         return Response({'message': 'File uploaded and processing started.'})
